f547b09d61256d694745dfac2d2ce323edc64cb6
Korwud <162795341+Korwud@users.noreply.github.com>
2024-05-20 14:10:39 +0500

    Add files via upload

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Grim_Castle.Architecture;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,52 +9,73 @@ namespace Grim_Castle
     {
         Texture2D tile_1;
         Texture2D tile_2;
+        Texture2D tile_3;
+        Texture2D tile_4;
+        Texture2D player;
+        Texture2D healthBar;
+        Map map;
+        Player basePlayer;
 
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        public SpriteBatch spriteBatch;
+        public SpriteFont font;
+
+        Vector2 position = Vector2.Zero;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferHeight = 980;
+            graphics.PreferredBackBufferWidth = 1550;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            basePlayer = new Player();
+            map = new Map();
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
             tile_1 = Content.Load<Texture2D>("tile_1");
             tile_2 = Content.Load<Texture2D>("tile_2");
-            // TODO: use this.Content to load your game content here
+            tile_3 = Content.Load<Texture2D>("tile_3");
+            tile_4 = Content.Load<Texture2D>("tile_4");
+            player = Content.Load<Texture2D>("player");
+            healthBar = Content.Load<Texture2D>("health bar");
+            font = Content.Load<SpriteFont>("HealthBar");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
+            MouseState currentMouseState = Mouse.GetState();
+            if (currentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                var (i, j) = map.FindCell(currentMouseState.X, currentMouseState.Y);
+                if (i != -1)
+                    position = map.CellPositions[i, j];
+                Draw(gameTime);
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(tile_1, new Vector2(0, 0), Color.White);
-            _spriteBatch.Draw(tile_2, new Vector2(76, 0), Color.White);
-            _spriteBatch.End();
-            // TODO: Add your drawing code here
+            MapDrawer.DrawMap(spriteBatch, tile_1, tile_2, tile_3, tile_4);
+            PlayerDrawer.PlayerDraw(spriteBatch, player, position);
+            spriteBatch.Begin();
+            spriteBatch.Draw(healthBar, new Vector2(5, 5), Color.White);
+            spriteBatch.DrawString(font, "5/20", new Vector2(100, 100), Color.Black);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
