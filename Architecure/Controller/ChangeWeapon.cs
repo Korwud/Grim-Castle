d696af4ca0b480a8ba4bfb2315b1d376e382a2c6
Korwud <162795341+Korwud@users.noreply.github.com>
2024-05-26 02:20:41 +0500

    Add files via upload

---
+++ Architecure/Controller/ChangeWeapon.cs
@@ -0,0 +1,21 @@
+﻿using Grim_Castle.Architecture.Model;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grim_Castle.Architecture.Controller
+{
+    public class ChangeWeapon
+    {
+        public static void Change(int x, int y)
+        {
+            var player = new Player();
+            var weapon = Inventory.GetWeaponFromCell(x, y);
+            if (weapon is not null)
+                player.ChangeCurrentWeapon(weapon);
+        }
+    }
+}
---
+++ Architecure/Controller/HitMonster.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grim_Castle.Architecture.Controller
+{
+    public class HitMonster
+    {
+        public static void Hit(Vector2 position)
+        {
+            var player = new Player();
+            var map = new Map();
+            var monster = map.IsMonsterHere(position);
+            if (monster is not null)
+            {
+                player.TakeDamage(monster);
+            }
+        }
+    }
+}
